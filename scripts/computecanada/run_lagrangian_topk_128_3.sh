be91687cdc0251bc80f232e00564b6e2e9b6b110
Raymond Li <li.raymond96@gmail.com>
2026-01-14 16:21:21 -0800

    included biredctional l0 constraint

---
+++ scripts/computecanada/run_lagrangian_topk_128_3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --account=def-carenini
+#SBATCH --time=6:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:h100:1
+#SBATCH --constraint=h100
+#SBATCH --job-name=lagrangian_l0_128_3
+
+module load StdEnv/2023
+module load python/3.12.4
+module load arrow/21.0.0
+module load cuda/12.6
+
+source ~/venvs/sae/bin/activate
+
+
+python run_sweep.py \
+--base_config configs/gpt2/gpt2-lagrangian.yaml \
+--sweep_config configs/gpt2/sweep/lagrangian_l0_128_3.yaml

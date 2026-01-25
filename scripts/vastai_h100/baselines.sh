748ebb342d969e5dd9987c7627b10a7951adadef
Raymond Li <li.raymond96@gmail.com>
2026-01-25 09:05:25 +0000

    all

--- scripts/vastai_h100/baselines.sh
+++ scripts/vastai_h100/baselines.sh
@@ -1,5 +1,9 @@
 CUDA_VISIBLE_DEVICES=3 python run_sweep.py \
 --base_config configs/gemma2-2b/gemma2-batchtopk.yaml \
+--sweep_config configs/gemma2-2b/sweep/65K_K_64.yaml
+
+CUDA_VISIBLE_DEVICES=3 python run_sweep.py \
+--base_config configs/gemma2-2b/gemma2-batchtopk.yaml \
 --sweep_config configs/gemma2-2b/sweep/65K_K_128.yaml
 
 CUDA_VISIBLE_DEVICES=3 python run_sweep.py \

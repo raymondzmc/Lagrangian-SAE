6d65876d53142f8f5f26e7d04d460df09b66427c
Raymond Li <li.raymond96@gmail.com>
2026-01-26 09:47:25 +0000

    all

--- scripts/vastai/gpt2/run_relu_gpt2.sh
+++ scripts/vastai/gpt2/run_relu_gpt2.sh
@@ -1,5 +1,5 @@
 conda activate sae
 
-CUDA_VISIBLE_DEVICES=0 python run_sweep.py \
+CUDA_VISIBLE_DEVICES=4 python run_sweep.py \
 --base_config configs/gpt2/gpt2-relu.yaml \
 --sweep_config configs/gpt2/sweep/relu_sweep.yaml

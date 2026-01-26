e868bb9b28010a644c5e9808508dd791cd2f4637
Raymond Li <li.raymond96@gmail.com>
2026-01-26 09:33:09 +0000

    gpt2

--- scripts/vastai/gemma2/run_matryoshka_lagrangian_gemma2_65K.sh
+++ scripts/vastai/gemma2/run_matryoshka_lagrangian_gemma2_65K.sh
@@ -1,5 +1,5 @@
 source /venv/sae/bin/activate
 
-CUDA_VISIBLE_DEVICES=3 python run_sweep.py \
+CUDA_VISIBLE_DEVICES=4 python run_sweep.py \
 --base_config configs/gemma2-2b/gemma2-matryoshka-lagrangian.yaml \
---sweep_config configs/gemma2-2b/sweep/lagrangian_sweeps/65K_l0_sweep_5.yaml
+--sweep_config configs/gemma2-2b/sweep/lagrangian_sweeps/65K_l0_sweep_6.yaml
---
+++ scripts/vastai/gpt2/run_lagrangian_gpt2_alpha10.sh
@@ -0,0 +1,5 @@
+conda activate sae
+
+CUDA_VISIBLE_DEVICES=0 python run_sweep.py \
+--base_config configs/gpt2/gpt2-lagrangian.yaml \
+--sweep_config configs/gpt2/sweep/lagrangian/lagrangian_l0_alpha_5.yaml
